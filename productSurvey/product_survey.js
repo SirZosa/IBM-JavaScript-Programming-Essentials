5131fb5903a7f7d478b36b6f1bf93a14ed8e9c94
Omar Sosa <omar.sosa.quimica@gmail.com>
2024-10-31 13:01:54 -0600

    product survey lab

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,32 @@
+function submitFeedback() {
+    alert('Thank you for your valuable feedback')
+    console.log(username)
+    document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+let username = document.getElementById('name').value;
+let age = document.getElementById('age').value;
+let email = document.getElementById('email').value;
+let job = document.getElementById('job').value;
+let designation = document.getElementById('designation').value;
+let productType = document.getElementById('productType').value;
+let feedback = document.getElementById('feedbackText').value;
+let submitButton=document.getElementById('submitBtn');
+submitButton.addEventListener('click', function(){
+    submitFeedback()
+})
+
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      submitFeedback();
+    }
+  });
